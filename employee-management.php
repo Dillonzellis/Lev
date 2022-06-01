678a44447b7cc8eee7b439fa80f70e6662acbdee
Dillon Ellis <dellis@fcul.com>
2022-06-01 13:42:44 -0400

    original emp mgmt page with text

---
+++ employee-management.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		
+		<title>LEVERAGE - Employee Management</title>
+		<link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.ico" />
+		<!-- Bootstrap -->
+		<link href="assets/css/bootstrap.css" rel="stylesheet">
+		<!-- font awesome -->
+		<link href="assets/css/font-awesome.css" rel="stylesheet">
+		<!-- aos -->
+		<link href="assets/css/aos.css" rel="stylesheet">
+		<!-- owl carousel -->
+		<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+		<!-- style sheet -->
+		<link href="assets/css/style.css" rel="stylesheet">
+		<!-- color stylesheet -->
+		<link id="skin-switcher-css" href="assets/css/color-1.css" rel="stylesheet">
+		<!-- responsive stylesheet -->
+		<link href="assets/css/responsive.css" rel="stylesheet">
+		
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-105142522-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-105142522-1');
+</script>
+		<script type="text/javascript">
+piAId = '514811';
+piCId = '168940';
+piHostname = 'pi.pardot.com';
+
+(function() {
+	function async_load(){
+		var s = document.createElement('script'); s.type = 'text/javascript';
+		s.src = ('https:' == document.location.protocol ? 'https://pi' : 'http://cdn') + '.pardot.com/pd.js';
+		var c = document.getElementsByTagName('script')[0]; c.parentNode.insertBefore(s, c);
+	}
+	if(window.attachEvent) { window.attachEvent('onload', async_load); }
+	else { window.addEventListener('load', async_load, false); }
+})();
+</script>
+
+	</head>
+	<body>
+		<!-- Header -->
+		<header>
+			<div class="nav_sec">
+				<div class="container">
+					<?php include 'assets/inc/nav.php';?>
+				</div>
+			</div>
+		</header>
+		<!-- Section -->
+		
+		<!-- Slider Sec -->
+		<section class="inner_banner">
+			<div class="container">
+				<h1>Employee Management</h1>
+				<ul class="breadcrum">
+					<li><a href="index.php">Home</a></li>
+					<li>Employee Management</li>
+				</ul>
+			</div>
+		</section>
+		<!-- About Sec -->
+		<section class="about_sec service_sec2">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 col-md-12">
+						
+						
+				<div class="row space-lg-3">
+					
+					<div class="col-lg-4 col-sm-6">
+						<div class="inner">
+							<a class="event" href="http://myleveragebenefits.com/" target="_blank">
+    							<img src="assets/images/core-solutions/leverage-benefits.png" alt="LEVERAGE Benegits Group"/>
+    
+    							<div class="content">
+        							<h3>LEVERAGE Benefits Group</h3>
+    
+       		 						<div class="rollover">
+            						<p>A partnership between LEVERAGE, affiliate of the League of Southeastern Credit Unions (LSCU) and Salus Group, an experienced benefits consulting firm among credit unions, offering a turnkey solution that provides comprehensive and competitive employee benefits and services at affordable prices.</p>
+        							</div>
+									
+    							</div>
+							</a>
+						</div>
+					</div>
+					
+				</div>
+						
+				<div class="row">
+					
+					<div class="col-lg-3 col-sm-6">
+						<div class="inner">
+							<a class="event" href="https://myleverage.com/solutions/cmg.php">
+    							<img src="assets/images/solutions/cmg-card.jpg" alt="CUNA Mutual Group"/>
+    
+    							<div class="content">
+        							<h3>CMG DEI</h3>
+    
+       		 						<div class="rollover">
+            						<p>A partnership between LEVERAGE, affiliate of the League of Southeastern Credit Unions (LSCU) and Salus Group, an experienced benefits consulting firm among credit unions, offering a turnkey solution that provides comprehensive and competitive employee benefits and services at affordable prices.</p>
+        							</div>
+									
+    							</div>
+							</a>
+						</div>
+					</div>
+					
+					<div class="col-lg-3 col-sm-6">
+						<div class="inner">
+							<a class="event" href="https://myleverage.com/solutions/cmg.php">
+    							<img src="assets/images/solutions/cmg-card.jpg" alt="CUNA Mutual Group"/>
+    
+    							<div class="content">
+        							<h3>Executive Benefits & Funding Solutions</h3>
+    
+       		 						<div class="rollover">
+            						<p>Content goes here.</p>
+        							</div>
+									
+    							</div>
+							</a>
+						</div>
+					</div>
+					
+					<div class="col-lg-3 col-sm-6">
+						<div class="inner">
+							<a class="event" href="https://myleverage.com/solutions/cmg.php">
+    							<img src="assets/images/solutions/cmg-card.jpg" alt="CUNA Mutual Group"/>
+    
+    							<div class="content">
+        							<h3>Retirement Solutions</h3>
+    
+       		 						<div class="rollover">
+            						<p>Content goes here.</p>
+        							</div>
+									
+    							</div>
+							</a>
+						</div>
+					</div>
+					
+					<div class="col-lg-3 col-sm-6">
+						<div class="inner">
+							<a class="event" href="solutions/landrumhr.php">
+    							<img src="assets/images/solutions/landrum-card.jpg" alt="HR Services"/>
+    
+    							<div class="content">
+        							<h3>LandrumHR</h3>
+    
+       		 						<div class="rollover">
+            						<p>Content goes here.</p>
+        							</div>
+									
+    							</div>
+							</a>
+						</div>
+					</div>
+					
+					
+					<div class="col-lg-3 col-sm-6">
+						<div class="inner">
+							<a class="event" href="solutions/lemonade.php">
+    							<img src="assets/images/solutions/lemonade-card.jpg" alt="LemonadeLXP"/>
+    
+    							<div class="content">
+        							<h3>LemonadeLXP</h3>
+    
+       		 						<div class="rollover">
+            						<p>An award-winning digital growth platform that helps credit unions nail their digital member experience.</p>
+        							</div>
+									
+    							</div>
+							</a>
+						</div>
+					</div>
+					
+					<div class="col-lg-3 col-sm-6">
+						<div class="inner">
+							<a class="event" href="solutions/lemonade.php">
+    							<img src="assets/images/solutions/lemonade-card.jpg" alt="LemonadeLXP"/>
+    
+    							<div class="content">
+        							<h3>LemonadeLXP</h3>
+    
+       		 						<div class="rollover">
+            						<p>An award-winning digital growth platform that helps credit unions nail their digital member experience.</p>
+        							</div>
+									
+    							</div>
+							</a>
+						</div>
+					</div>
+					
+					<div class="col-lg-3 col-sm-6">
+						<div class="inner">
+							<a class="event" href="solutions/lemonade.php">
+    							<img src="assets/images/solutions/lemonade-card.jpg" alt="LemonadeLXP"/>
+    
+    							<div class="content">
+        							<h3>LemonadeLXP</h3>
+    
+       		 						<div class="rollover">
+            						<p>An award-winning digital growth platform that helps credit unions nail their digital member experience.</p>
+        							</div>
+									
+    							</div>
+							</a>
+						</div>
+					</div>
+					
+					<div class="col-lg-3 col-sm-6">
+						<div class="inner">
+							<a class="event" href="solutions/lemonade.php">
+    							<img src="assets/images/solutions/lemonade-card.jpg" alt="LemonadeLXP"/>
+    
+    							<div class="content">
+        							<h3>LemonadeLXP</h3>
+    
+       		 						<div class="rollover">
+            						<p>An award-winning digital growth platform that helps credit unions nail their digital member experience.</p>
+        							</div>
+									
+    							</div>
+							</a>
+						</div>
+					</div>
+					
+				</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- Service Sec -->
+		
+		<!-- Footer -->
+		<footer>
+			<section class="footer_detail footer_detail_3">
+				<?php include 'assets/inc/footer.php';?>
+			</section>
+		</footer>
+		<!-- Js -->
+		<script src="assets/js/jquery-3.1.0.min.js"></script>
+		<script src="assets/js/popper.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
+		<script src="assets/js/owl.carousel.js"></script>
+		<!-- theme Whatsapp chat -->
+		<script src="assets/js/aos.js"></script>
+		<script src="assets/js/script.js"></script>
+	</body>
+</html>
