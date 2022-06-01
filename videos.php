8248b738c52a894871c124b67b67993b7a55ebc3
Dillon Ellis <dellis@fcul.com>
2022-06-01 13:38:26 -0400

    add new videos

--- videos.php
+++ videos.php
@@ -1,144 +1,199 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		
-		<title>LEVERAGE - Videos</title>
-		<link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.ico" />
-		<!-- Bootstrap -->
-		<link href="assets/css/bootstrap.css" rel="stylesheet">
-		<!-- font awesome -->
-		<link href="assets/css/font-awesome.css" rel="stylesheet">
-		<!-- aos -->
-		<link href="assets/css/aos.css" rel="stylesheet">
-		<!-- owl carousel -->
-		<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
-		<!-- style sheet -->
-		<link href="assets/css/style.css" rel="stylesheet">
-		<!-- color stylesheet -->
-		<link id="skin-switcher-css" href="assets/css/color-1.css" rel="stylesheet">
-		<!-- responsive stylesheet -->
-		<link href="assets/css/responsive.css" rel="stylesheet">
-		
-		<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-105142522-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'UA-105142522-1');
-</script>
-		<script type="text/javascript">
-piAId = '514811';
-piCId = '168940';
-piHostname = 'pi.pardot.com';
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-(function() {
-	function async_load(){
-		var s = document.createElement('script'); s.type = 'text/javascript';
-		s.src = ('https:' == document.location.protocol ? 'https://pi' : 'http://cdn') + '.pardot.com/pd.js';
-		var c = document.getElementsByTagName('script')[0]; c.parentNode.insertBefore(s, c);
-	}
-	if(window.attachEvent) { window.attachEvent('onload', async_load); }
-	else { window.addEventListener('load', async_load, false); }
-})();
-</script>
+    <title>LEVERAGE - Videos</title>
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.ico" />
+    <!-- Bootstrap -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <!-- font awesome -->
+    <link href="assets/css/font-awesome.css" rel="stylesheet">
+    <!-- aos -->
+    <link href="assets/css/aos.css" rel="stylesheet">
+    <!-- owl carousel -->
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <!-- style sheet -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <!-- color stylesheet -->
+    <link id="skin-switcher-css" href="assets/css/color-1.css" rel="stylesheet">
+    <!-- responsive stylesheet -->
+    <link href="assets/css/responsive.css" rel="stylesheet">
 
-	</head>
-	<body>
-		<!-- Header -->
-		<header>
-			<div class="nav_sec">
-				<div class="container">
-					<?php include 'assets/inc/nav.php';?>
-				</div>
-			</div>
-		</header>
-		<!-- Section -->
-		
-		<!-- Slider Sec -->
-		<section class="inner_banner">
-			<div class="container">
-				<h1>Videos</h1>
-				<ul class="breadcrum">
-					<li><a href="index.php">Home</a></li>
-					<li>Videos</li>
-				</ul>
-			</div>
-		</section>
-		<!-- About Sec -->
-		<section class="about_sec service_sec2">
-			<div class="container">
-				<div class="row">
-						
-						<div class="col-lg-6 col-sm-6">
-							<iframe width="560" height="315" src="https://www.youtube.com/embed/ajA9XzLcrY8" title="LEVERAGE Testimonials" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><strong><em>LEVERAGE</em> Testimonials</strong>
-						</div>
-						
-						<div class="col-lg-6 col-sm-6">
-						<iframe width="560" height="315" src="https://www.youtube.com/embed/mhLChRdVTHg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen title="Virtual StrongBox and RecordLyf"></iframe><br><strong>Virtual StrongBox and RecordLyf</strong>
-					    </div>
-					
-					<div class="col-lg-12">&nbsp;</div>
-					
-					<div class="col-lg-6 col-sm-6">
-						<iframe width="560" height="315" src="https://www.youtube.com/embed/dOFO1K0yrMU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen title="Members ATM Alliance"></iframe><br><strong>Members ATM Alliance</strong>
-					    </div>
-					
-					<div class="col-lg-6 col-sm-6">
-						<iframe width="560" height="315" src="https://www.youtube.com/embed/JRXBGXhiNXM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen title="Complete CECL Solution - CRF Advisors Inc."></iframe><br><strong>Complete CECL Solution - CRF Advisors, Inc.</strong>
-					    </div>
-					
-					<div class="col-lg-12">&nbsp;</div>
-					
-					<div class="col-lg-6 col-sm-6">
-						<iframe title="payment solutions video" width="560" height="315" src="https://www.youtube.com/embed/w0lo292mZ9c" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><strong>LEVERAGE Payment Solutions</strong>
-					    </div>
-					
-					<div class="col-lg-6 col-sm-6">
-						<iframe width="560" height="315" src="https://www.youtube.com/embed/YdwCbqLWX9U" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen title="Shared Branching"></iframe><br><strong>Shared Branching</strong>
-					    </div>
-					
-					<div class="col-lg-12">&nbsp;</div>
-					
-					<div class="col-lg-6 col-sm-6">
-						<iframe width="560" height="315" src="https://www.youtube.com/embed/W4wqbDEdeF8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><strong>DDI Technology</strong>
-					    </div>
-					
-					<div class="col-lg-6 col-sm-6">
-						<iframe width="560" height="315" src="https://www.youtube.com/embed/rjcm7jWilw8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen title="LEVERAGE Access Softek"></iframe><br><strong>Access Softek</strong>
-					    </div>
-					
-					<div class="col-lg-12">&nbsp;</div>
-					
-					<div class="col-lg-6 col-sm-6">
-						<iframe width="560" height="315" src="https://www.youtube.com/embed/xQScLTMWk6g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><strong>Glia</strong>
-					    </div>
-					
-					<div class="col-lg-6 col-sm-6">
-						&nbsp;
-					    </div>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-105142522-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
 
-				</div>
-			</div>
-		</section>
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
 
-		<!-- Footer -->
-		<footer>
-			<section class="footer_detail footer_detail_3">
-				<?php include 'assets/inc/footer.php';?>
-			</section>
-		</footer>
-		<!-- Js -->
-		<script src="assets/js/jquery-3.1.0.min.js"></script>
-		<script src="assets/js/popper.min.js"></script>
-		<script src="assets/js/bootstrap.min.js"></script>
-		<script src="assets/js/owl.carousel.js"></script>
-		<!-- theme Whatsapp chat -->
-		<script src="assets/js/aos.js"></script>
-		<script src="assets/js/script.js"></script>
-	</body>
+        gtag('config', 'UA-105142522-1');
+    </script>
+    <script type="text/javascript">
+        piAId = '514811';
+        piCId = '168940';
+        piHostname = 'pi.pardot.com';
+
+        (function() {
+            function async_load() {
+                var s = document.createElement('script');
+                s.type = 'text/javascript';
+                s.src = ('https:' == document.location.protocol ? 'https://pi' : 'http://cdn') + '.pardot.com/pd.js';
+                var c = document.getElementsByTagName('script')[0];
+                c.parentNode.insertBefore(s, c);
+            }
+            if (window.attachEvent) {
+                window.attachEvent('onload', async_load);
+            } else {
+                window.addEventListener('load', async_load, false);
+            }
+        })();
+    </script>
+
+</head>
+
+<body>
+    <!-- Header -->
+    <header>
+        <div class="nav_sec">
+            <div class="container">
+                <?php include 'assets/inc/nav.php'; ?>
+            </div>
+        </div>
+    </header>
+    <!-- Section -->
+
+    <!-- Slider Sec -->
+    <section class="inner_banner">
+        <div class="container">
+            <h1>Videos</h1>
+            <ul class="breadcrum">
+                <li><a href="index.php">Home</a></li>
+                <li>Videos</li>
+            </ul>
+        </div>
+    </section>
+    <!-- About Sec -->
+    <section class="about_sec service_sec2">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-6 col-sm-6">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/ajA9XzLcrY8" title="LEVERAGE Testimonials" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><strong><em>LEVERAGE</em> Testimonials</strong>
+                </div>
+
+                <div class="col-lg-6 col-sm-6">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/mhLChRdVTHg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen title="Virtual StrongBox and RecordLyf"></iframe><br><strong>Virtual StrongBox and RecordLyf</strong>
+                </div>
+
+                <div class="col-lg-12">&nbsp;</div>
+
+                <div class="col-lg-6 col-sm-6">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/dOFO1K0yrMU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen title="Members ATM Alliance"></iframe><br><strong>Members ATM Alliance</strong>
+                </div>
+
+                <div class="col-lg-6 col-sm-6">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/JRXBGXhiNXM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen title="Complete CECL Solution - CRF Advisors Inc."></iframe><br><strong>Complete CECL Solution - CRF Advisors, Inc.</strong>
+                </div>
+
+                <div class="col-lg-12">&nbsp;</div>
+
+                <div class="col-lg-6 col-sm-6">
+                    <iframe title="payment solutions video" width="560" height="315" src="https://www.youtube.com/embed/w0lo292mZ9c" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><strong>LEVERAGE Payment Solutions</strong>
+                </div>
+
+                <div class="col-lg-6 col-sm-6">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/YdwCbqLWX9U" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen title="Shared Branching"></iframe><br><strong>Shared Branching</strong>
+                </div>
+
+                <div class="col-lg-12">&nbsp;</div>
+
+                <div class="col-lg-6 col-sm-6">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/W4wqbDEdeF8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><strong>DDI Technology</strong>
+                </div>
+
+                <div class="col-lg-6 col-sm-6">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/rjcm7jWilw8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen title="LEVERAGE Access Softek"></iframe><br><strong>Access Softek</strong>
+                </div>
+
+                <div class="col-lg-12">&nbsp;</div>
+
+                <div class="col-lg-6 col-sm-6">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/xQScLTMWk6g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><strong>Glia</strong>
+                </div>
+
+                <div class="col-lg-6 col-sm-6">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/2p_wbBPBLj4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><strong>Commercial Alliance</strong>
+                </div>
+
+                <div class="col-lg-6 col-sm-6">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/na5iHmJIB1o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><strong>CUFS</strong>
+                </div>
+
+                <div class="col-lg-6 col-sm-6">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/HEbjYIHnfM8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><strong>OSG</strong>
+                </div>
+
+                <div class="col-lg-6 col-sm-6">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/u31B2_9yEzU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><strong>Synergi Partners & LEVERAGE ERC Webinar</strong>
+                </div>
+
+                <div class="col-lg-6 col-sm-6">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/aAfcVdu9Sd0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><strong>Credit Union Audit and Compliance Group</strong>
+                </div>
+
+                <div class="col-lg-6 col-sm-6">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/kEH3xh-101I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><strong>Tru Treasury feature for LEVERAGE Monthly January 2021</strong>
+                </div>
+
+                <div class="col-lg-6 col-sm-6">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/GH30uVnYA1U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><strong>Working with LEVERAGE</strong>
+                </div>
+
+                <div class="col-lg-6 col-sm-6">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/Dg1u9huU3rA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><strong>Virtual StrongBox</strong>
+                </div>
+
+                <div class="col-lg-6 col-sm-6">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/D7i682-862s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><strong>DDI</strong>
+                </div>
+
+                <div class="col-lg-6 col-sm-6">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/62IE7JRHx7g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><strong>Your Check Printing Consultant</strong>
+                </div>
+
+
+
+
+
+                <div class="col-lg-6 col-sm-6">
+                    &nbsp;
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <footer>
+        <section class="footer_detail footer_detail_3">
+            <?php include 'assets/inc/footer.php'; ?>
+        </section>
+    </footer>
+    <!-- Js -->
+    <script src="assets/js/jquery-3.1.0.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/owl.carousel.js"></script>
+    <!-- theme Whatsapp chat -->
+    <script src="assets/js/aos.js"></script>
+    <script src="assets/js/script.js"></script>
+</body>
+
 </html>
