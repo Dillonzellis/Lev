9b3f11eba8913257b7da870d0dae7fcd7f7de59d
Dillon Ellis <dellis@fcul.com>
2022-06-01 14:02:49 -0400

    bootstrap 5 new stylesheet and card formatting

--- testing-de.php
+++ testing-de.php
@@ -9,7 +9,11 @@
     <title>LEVERAGE - Employee Management</title>
     <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.ico" />
     <!-- Bootstrap -->
-    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <!-- <link href="assets/css/bootstrap.css" rel="stylesheet"> -->
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     <!-- font awesome -->
     <link href="assets/css/font-awesome.css" rel="stylesheet">
     <!-- aos -->
@@ -22,6 +26,8 @@
     <link id="skin-switcher-css" href="assets/css/color-1.css" rel="stylesheet">
     <!-- responsive stylesheet -->
     <link href="assets/css/responsive.css" rel="stylesheet">
+	
+	<link href="assets/css/de-style.css" rel="stylesheet">
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-105142522-1"></script>
@@ -56,6 +62,7 @@
         })();
     </script>
 
+	
 </head>
 
 <body>
@@ -86,7 +93,7 @@
                 <div class="col-lg-12 col-md-12">
 
 
-                    <div class="row space-lg-3">
+                    <div class="row space-lg-3 justify-content-center mb-5">
                         <div class="col-lg-4 col-sm-6">
                             <div class="inner">
                                 <a class="event" href="http://myleveragebenefits.com/" target="_blank">
