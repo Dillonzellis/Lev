08864d107732c204d351d4c97053b27103736bfa
Dillon Ellis <dellis@fcul.com>
2022-06-02 16:16:15 -0400

    liquidity page

---
+++ liquidity.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>LEVERAGE - Liquidity</title>
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.ico" />
+    <!-- Bootstrap -->
+    <!-- <link href="assets/css/bootstrap.css" rel="stylesheet"> -->
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <!-- font awesome -->
+    <link href="assets/css/font-awesome.css" rel="stylesheet">
+    <!-- aos -->
+    <link href="assets/css/aos.css" rel="stylesheet">
+    <!-- owl carousel -->
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <!-- style sheet -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <!-- color stylesheet -->
+    <link id="skin-switcher-css" href="assets/css/color-1.css" rel="stylesheet">
+    <!-- responsive stylesheet -->
+    <link href="assets/css/responsive.css" rel="stylesheet">
+
+    <link href="assets/css/de-style.css" rel="stylesheet">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-105142522-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-105142522-1');
+    </script>
+    <script type="text/javascript">
+        piAId = '514811';
+        piCId = '168940';
+        piHostname = 'pi.pardot.com';
+
+        (function() {
+            function async_load() {
+                var s = document.createElement('script');
+                s.type = 'text/javascript';
+                s.src = ('https:' == document.location.protocol ? 'https://pi' : 'http://cdn') + '.pardot.com/pd.js';
+                var c = document.getElementsByTagName('script')[0];
+                c.parentNode.insertBefore(s, c);
+            }
+            if (window.attachEvent) {
+                window.attachEvent('onload', async_load);
+            } else {
+                window.addEventListener('load', async_load, false);
+            }
+        })();
+    </script>
+
+
+</head>
+
+<body>
+    <!-- Header -->
+    <header>
+        <div class="nav_sec">
+            <div class="container">
+                <?php include 'assets/inc/nav.php'; ?>
+            </div>
+        </div>
+    </header>
+    <!-- Section -->
+
+    <!-- Slider Sec -->
+    <section class="inner_banner">
+        <div class="container">
+            <h1>Liquidity</h1>
+            <ul class="breadcrum">
+                <li><a href="index.php">Home</a></li>
+                <li>Liquidity</li>
+            </ul>
+        </div>
+    </section>
+
+    <section class="secondary-cards">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="https://myleverage.com/solutions/culs.php">
+                            <img src="assets/images/solutions/culs-card.png" alt="Credit Union Loan Source (CULS)" />
+                            <div class="content">
+                                <h3>Credit Union Loan Source (CULS)</h3>
+                                <div class="rollover">
+                                    <p>CULS’ deep experience and expertise in the auto lending industry.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="https://www.trutreasury.com/">
+                            <img src="assets/images/solutions/tru-card.png" alt="Tru Treasury" />
+                            <div class="content">
+                                <h3>Tru Treasury</h3>
+                                <div class="rollover">
+                                    <p>Managing funds and accounts while protecting member relationships and identifying opportunities.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="https://myleverage.com/solutions/cmg.php">
+                            <img src="assets/images/solutions/cmg-card.jpg" alt="CMG Investments" />
+                            <div class="content">
+                                <h3>CMG Investments</h3>
+                                <div class="rollover">
+                                    <p>Services such as financial advisement on annuities, brokerage services, commercial mortgage loans, and fiduciary consulting.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="https://myleverage.com/solutions/commercial-alliance.php">
+                            <img src="assets/images/solutions/com-alliance-card.png" alt="Commercial Alliance" />
+                            <div class="content">
+                                <h3>Commercial Alliance</h3>
+                                <div class="rollover">
+                                    <p>A comprehensive service designed to help credit unions excel in Member Business Lending(MBL).</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="https://myleverage.com/solutions/mbfs.php">
+                            <img src="assets/images/solutions/mbfs-card.png" alt="Member Business Financial Services (MBFS)" />
+                            <div class="content">
+                                <h3>Member Business Financial Services (MBFS)</h3>
+                                <div class="rollover">
+                                    <p>MBFS assists credit unions to fund business loans.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="https://myleverage.com/solutions/passtime.php">
+                            <img src="assets/images/solutions/passtime-card.png" alt="Stifel" />
+                            <div class="content">
+                                <h3>Stifel</h3>
+                                <div class="rollover">
+                                    <p>info to come.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <footer>
+        <section class="footer_detail footer_detail_3">
+            <?php include 'assets/inc/footer.php'; ?>
+        </section>
+    </footer>
+    <!-- Js -->
+    <script src="assets/js/jquery-3.1.0.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/owl.carousel.js"></script>
+    <!-- theme Whatsapp chat -->
+    <script src="assets/js/aos.js"></script>
+    <script src="assets/js/script.js"></script>
+</body>
+
+</html>
