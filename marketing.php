dea8d17a7eb8fe4dac2fc2702a5f67c72f5491eb
Dillon Ellis <dellis@fcul.com>
2022-06-02 16:30:35 -0400

    marketing page

---
+++ marketing.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>LEVERAGE - Marketing</title>
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.ico" />
+    <!-- Bootstrap -->
+    <!-- <link href="assets/css/bootstrap.css" rel="stylesheet"> -->
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <!-- font awesome -->
+    <link href="assets/css/font-awesome.css" rel="stylesheet">
+    <!-- aos -->
+    <link href="assets/css/aos.css" rel="stylesheet">
+    <!-- owl carousel -->
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <!-- style sheet -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <!-- color stylesheet -->
+    <link id="skin-switcher-css" href="assets/css/color-1.css" rel="stylesheet">
+    <!-- responsive stylesheet -->
+    <link href="assets/css/responsive.css" rel="stylesheet">
+
+    <link href="assets/css/de-style.css" rel="stylesheet">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-105142522-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-105142522-1');
+    </script>
+    <script type="text/javascript">
+        piAId = '514811';
+        piCId = '168940';
+        piHostname = 'pi.pardot.com';
+
+        (function() {
+            function async_load() {
+                var s = document.createElement('script');
+                s.type = 'text/javascript';
+                s.src = ('https:' == document.location.protocol ? 'https://pi' : 'http://cdn') + '.pardot.com/pd.js';
+                var c = document.getElementsByTagName('script')[0];
+                c.parentNode.insertBefore(s, c);
+            }
+            if (window.attachEvent) {
+                window.attachEvent('onload', async_load);
+            } else {
+                window.addEventListener('load', async_load, false);
+            }
+        })();
+    </script>
+
+
+</head>
+
+<body>
+    <!-- Header -->
+    <header>
+        <div class="nav_sec">
+            <div class="container">
+                <?php include 'assets/inc/nav.php'; ?>
+            </div>
+        </div>
+    </header>
+    <!-- Section -->
+
+    <!-- Slider Sec -->
+    <section class="inner_banner">
+        <div class="container">
+            <h1>Marketing</h1>
+            <ul class="breadcrum">
+                <li><a href="index.php">Home</a></li>
+                <li>Marketing</li>
+            </ul>
+        </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-4 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="http://www.growthbydesign.org" target="_blank">
+                            <img src="assets/images/core-solutions/gbd.png" alt="Growth by Design">
+                            <div class="content">
+                                <h3>Marketing Services</h3>
+                                <div class="rollover">
+                                    <p>We are passionate and experienced designers, developers, artists, writers, videographers, editors, photographers, producers and strategists. Through our individual callings to imagine, discover and DO, we bring our talents together to create remarkable results for clients and unmatched experiences for audiences.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="secondary-cards">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="https://myleverage.com/solutions/cmg.php">
+                            <img src="assets/images/solutions/cmg-card.jpg" alt="CMG AdvantageEdgeTM Digital" />
+                            <div class="content">
+                                <h3>CMG AdvantageEdgeTM Digital</h3>
+                                <div class="rollover">
+                                    <p>A suite of SaaS solutions for digital lending and financial performance management analytics.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="https://myleverage.com/solutions/element.php">
+                            <img src="assets/images/solutions/element-card.png" alt="Element" />
+                            <div class="content">
+                                <h3>Element</h3>
+                                <div class="rollover">
+                                    <p>With Element, credit unions can convert outdated financial spaces into dynamic experiences that enhance member engagement.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="solutions/lemonade.php">
+                            <img src="assets/images/solutions/lemonade-card.jpg" alt="LemonadeLXP" />
+                            <div class="content">
+                                <h3>LemonadeLXP</h3>
+                                <div class="rollover">
+                                    <p>An award-winning digital growth platform that helps credit unions nail their digital member experience.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="https://myleverage.com/solutions/osg.php">
+                            <img src="assets/images/solutions/osg-card.png" alt="OSG" />
+                            <div class="content">
+                                <h3>OSG</h3>
+                                <div class="rollover">
+                                    <p>Global provider of leading customer engagement and payment solutions. Modernized processes, frictionless experiences.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="https://myleverage.com/solutions/css.php">
+                            <img src="assets/images/solutions/2020-card.png" alt="20/20 Analytics (CSS)" />
+                            <div class="content">
+                                <h3>20/20 Analytics (CSS)</h3>
+                                <div class="rollover">
+                                    <p>Global provider of leading customer engagement and payment solutions. Modernized processes, frictionless experiences.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+
+
+
+
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <footer>
+        <section class="footer_detail footer_detail_3">
+            <?php include 'assets/inc/footer.php'; ?>
+        </section>
+    </footer>
+    <!-- Js -->
+    <script src="assets/js/jquery-3.1.0.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/owl.carousel.js"></script>
+    <!-- theme Whatsapp chat -->
+    <script src="assets/js/aos.js"></script>
+    <script src="assets/js/script.js"></script>
+</body>
+
+</html>
