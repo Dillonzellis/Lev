aebcca5d91bd39d849de33ef20ffba02d7de917f
Dillon Ellis <dellis@fcul.com>
2022-06-03 10:55:47 -0400

    risk page

---
+++ risk-management.php
@@ -0,0 +1,409 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>LEVERAGE - Risk Management</title>
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.ico" />
+    <!-- Bootstrap -->
+    <!-- <link href="assets/css/bootstrap.css" rel="stylesheet"> -->
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <!-- font awesome -->
+    <link href="assets/css/font-awesome.css" rel="stylesheet">
+    <!-- aos -->
+    <link href="assets/css/aos.css" rel="stylesheet">
+    <!-- owl carousel -->
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <!-- style sheet -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <!-- color stylesheet -->
+    <link id="skin-switcher-css" href="assets/css/color-1.css" rel="stylesheet">
+    <!-- responsive stylesheet -->
+    <link href="assets/css/responsive.css" rel="stylesheet">
+
+    <link href="assets/css/de-style.css" rel="stylesheet">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-105142522-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-105142522-1');
+    </script>
+    <script type="text/javascript">
+        piAId = '514811';
+        piCId = '168940';
+        piHostname = 'pi.pardot.com';
+
+        (function() {
+            function async_load() {
+                var s = document.createElement('script');
+                s.type = 'text/javascript';
+                s.src = ('https:' == document.location.protocol ? 'https://pi' : 'http://cdn') + '.pardot.com/pd.js';
+                var c = document.getElementsByTagName('script')[0];
+                c.parentNode.insertBefore(s, c);
+            }
+            if (window.attachEvent) {
+                window.attachEvent('onload', async_load);
+            } else {
+                window.addEventListener('load', async_load, false);
+            }
+        })();
+    </script>
+
+
+</head>
+
+<body>
+    <!-- Header -->
+    <header>
+        <div class="nav_sec">
+            <div class="container">
+                <?php include 'assets/inc/nav.php'; ?>
+            </div>
+        </div>
+    </header>
+    <!-- Section -->
+
+    <!-- Slider Sec -->
+    <section class="inner_banner">
+        <div class="container">
+            <h1>Risk Management</h1>
+            <ul class="breadcrum">
+                <li><a href="index.php">Home</a></li>
+                <li>Risk Management</li>
+            </ul>
+        </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <div class="row justify-content-center">
+
+                <div class="col-lg-4 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="http://cuacg.com/" target="_blank">
+                            <img src="assets/images/core-solutions/cuacg.png" alt="Compliance" />
+                            <div class="content">
+                                <h3>Audit & Compliance</h3>
+                                <div class="rollover">
+                                    <p>Credit Union Audit and Compliance Group (CUACG), the largest credit union audit and compliance firm within the Southeast, provides expertise to help credit unions navigate the changing regulatory and compliance environment.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+
+                <div class="col-lg-4 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="http://www.cuvm.org/" target="_blank">
+                            <img src="assets/images/core-solutions/cuvm.png" alt="Credit Union Vendor Management" />
+
+                            <div class="content">
+                                <h3>Vendor Management</h3>
+
+                                <div class="rollover">
+                                    <p>CUVM is a vendor management solution that provides credit unions with a cost effective Vendor Management module. CUVM, originally created as a Colorado CUSO in 2008, was purchased in 2012 by LEVERAGE, Credit Union Solutions Group, and Mountain West Credit Union Association to enhance the existing Self Service platform by adding a Full Service option.</p>
+                                </div>
+
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+
+                <div class="col-lg-4 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="http://sharedcooperativeservices.com/" target="_blank">
+                            <img src="assets/images/core-solutions/scs.png" alt="Shared Cooperative Services" />
+
+                            <div class="content">
+                                <h3>Shared Cooperative Services</h3>
+
+                                <div class="rollover">
+                                    <p>Shared Cooperative Services is the LSCU & Affiliates subsidiary for shared branching with 83 participating credit unions operating 350 outlets. Shared Cooperative Services is the state network for Alabama and Georgia and is part of the national network, CO-OP Shared Branch.</p>
+                                </div>
+
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+
+
+            </div>
+        </div>
+    </section>
+
+    <section class="secondary-cards">
+        <div class="container">
+            <div class="row">
+
+
+
+            <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="https://www.trutreasury.com/">
+                            <img src="assets/images/solutions/tru-card.png" alt="Tru Treasury" />
+                            <div class="content">
+                                <h3>Tru Treasury</h3>
+                                <div class="rollover">
+                                    <p>Managing funds and accounts while protecting member relationships and identifying opportunities.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="https://myleverage.com/solutions/cmg.php">
+                            <img src="assets/images/solutions/cmg-card.jpg" alt="CMG Credit Union Protection" />
+                            <div class="content">
+                                <h3>CMG Credit Union Protection</h3>
+                                <div class="rollover">
+                                    <p>Managing funds and accounts while protecting member relationships and identifying opportunities.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="https://myleverage.com/solutions/cmg.php">
+                            <img src="assets/images/solutions/cmg-card.jpg" alt="CMG TruStageR Insurance" />
+                            <div class="content">
+                                <h3>CMG TruStageR Insurance</h3>
+                                <div class="rollover">
+                                    <p>Products include Accidental Death & Dismemberment (AD&D), Life, Auto & Home insurance.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="http://www.crfadvisors.com/">
+                            <img src="assets/images/solutions/crf-card.png" alt="CRF Advisors" />
+                            <div class="content">
+                                <h3>CRF Advisors</h3>
+                                <div class="rollover">
+                                    <p>A risk management firm that services financial institutions, non-profit organizations, and financial services companies.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="https://myleverage.com/solutions/overdraft-protection.php">
+                            <img src="assets/images/solutions/jmfa-card.png" alt="JMFA Overdraft Protection" />
+                            <div class="content">
+                                <h3>JMFA Overdraft Protection</h3>
+                                <div class="rollover">
+                                    <p>Earn non-interest income and provide a quality overdraft protection program to your members.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="https://www.membersecuritycenter.com/">
+                            <img src="assets/images/solutions/mbsc-card.png" alt="Member Security Center" />
+                            <div class="content">
+                                <h3>Member Security Center</h3>
+                                <div class="rollover">
+                                    <p>Member Security Center increases non-interest income and strengthens member relationships.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="https://myleverage.com/solutions/par.php">
+                            <img src="assets/images/solutions/par-card.png" alt="Partners in Asset Recovery (PAR)" />
+                            <div class="content">
+                                <h3>Partners in Asset Recovery (PAR)</h3>
+                                <div class="rollover">
+                                    <p>Unparalleled nationwide total asset management from start to finish.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="https://myleverage.com/solutions/passtime.php">
+                            <img src="assets/images/solutions/passtime-card.png" alt="PassTime GPS" />
+                            <div class="content">
+                                <h3>PassTime GPS</h3>
+                                <div class="rollover">
+                                    <p>Maximizing automated collection technology.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="http://www.pivotgroup.com/">
+                            <img src="assets/images/solutions/pivot-card.png" alt="PIVOT Group" />
+                            <div class="content">
+                                <h3>PIVOT Group</h3>
+                                <div class="rollover">
+                                    <p>Cyber Security Audit and Compliance Services.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="https://myleverage.com/solutions/recordlyf.php">
+                            <img src="assets/images/solutions/record-card.png" alt="RecordLyf" />
+                            <div class="content">
+                                <h3>RecordLyf</h3>
+                                <div class="rollover">
+                                    <p>Protect your valuable personal information.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="https://www.virtualstrongbox.com/">
+                            <img src="assets/images/solutions/virtual-strong-box-card.png" alt="Virtual Strong Box" />
+                            <div class="content">
+                                <h3>Virtual Strong Box</h3>
+                                <div class="rollover">
+                                    <p>A secure platform that addresses the balance of data security and consumer convenience.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="https://myleverage.com/solutions/css.php">
+                            <img src="assets/images/solutions/2020-card.png" alt="20/20 Analytics (CSS)" />
+                            <div class="content">
+                                <h3>20/20 Analytics (CSS)</h3>
+                                <div class="rollover">
+                                    <p>Global provider of leading customer engagement and payment solutions. Modernized processes, frictionless experiences.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="https://myleverage.com/solutions/css.php">
+                            <img src="assets/images/solutions/agility-card.png" alt="Agility Recovery" />
+                            <div class="content">
+                                <h3>Agility Recovery</h3>
+                                <div class="rollover">
+                                    <p>Through its business continuity management platform, called Agility Central, Agility works to reduce the impact of business interruptions on credit unions and the communities they serve.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="#">
+                            <img src="assets/images/solutions/illumalogo-card.png" alt="ComplySight (CSS)" />
+                            <div class="content">
+                                <h3>ComplySight (CSS)</h3>
+                                <div class="rollover">
+                                    <p>info to come</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="https://myleverage.com/solutions/css.php">
+                            <img src="assets/images/solutions/illumalogo-card.png" alt="Illuma Lab" />
+                            <div class="content">
+                                <h3>Illuma Labs</h3>
+                                <div class="rollover">
+                                    <p>Illuma, a Credit Union Service Organization (CUSO), specializes in voice authentication software that replaces traditional knowledge-based authentication practices in call centers.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="https://myleverage.com/solutions/css.php">
+                            <img src="assets/images/solutions/verafin-card.png" alt="Verafin (CSS)" />
+                            <div class="content">
+                                <h3>Verafin (CSS)</h3>
+                                <div class="rollover">
+                                    <p>Verafin is the industry leader in enterprise financial crime management, providing a cloud-based, secure software platform for fraud detection and management, BSA/AML compliance and management, high-risk customer management and information sharing.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+
+
+
+
+
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <footer>
+        <section class="footer_detail footer_detail_3">
+            <?php include 'assets/inc/footer.php'; ?>
+        </section>
+    </footer>
+    <!-- Js -->
+    <script src="assets/js/jquery-3.1.0.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/owl.carousel.js"></script>
+    <!-- theme Whatsapp chat -->
+    <script src="assets/js/aos.js"></script>
+    <script src="assets/js/script.js"></script>
+</body>
+
+</html>
