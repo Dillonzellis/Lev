7dc8fd29b3e4ee7a6d2a8194084d500c469e9875
Dillon Ellis <dellis@fcul.com>
2022-06-03 09:41:17 -0400

    noninterest page

---
+++ noninterest-income.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>LEVERAGE - Non-Interest Income</title>
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.ico" />
+    <!-- Bootstrap -->
+    <!-- <link href="assets/css/bootstrap.css" rel="stylesheet"> -->
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <!-- font awesome -->
+    <link href="assets/css/font-awesome.css" rel="stylesheet">
+    <!-- aos -->
+    <link href="assets/css/aos.css" rel="stylesheet">
+    <!-- owl carousel -->
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <!-- style sheet -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <!-- color stylesheet -->
+    <link id="skin-switcher-css" href="assets/css/color-1.css" rel="stylesheet">
+    <!-- responsive stylesheet -->
+    <link href="assets/css/responsive.css" rel="stylesheet">
+
+    <link href="assets/css/de-style.css" rel="stylesheet">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-105142522-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-105142522-1');
+    </script>
+    <script type="text/javascript">
+        piAId = '514811';
+        piCId = '168940';
+        piHostname = 'pi.pardot.com';
+
+        (function() {
+            function async_load() {
+                var s = document.createElement('script');
+                s.type = 'text/javascript';
+                s.src = ('https:' == document.location.protocol ? 'https://pi' : 'http://cdn') + '.pardot.com/pd.js';
+                var c = document.getElementsByTagName('script')[0];
+                c.parentNode.insertBefore(s, c);
+            }
+            if (window.attachEvent) {
+                window.attachEvent('onload', async_load);
+            } else {
+                window.addEventListener('load', async_load, false);
+            }
+        })();
+    </script>
+
+
+</head>
+
+<body>
+    <!-- Header -->
+    <header>
+        <div class="nav_sec">
+            <div class="container">
+                <?php include 'assets/inc/nav.php'; ?>
+            </div>
+        </div>
+    </header>
+    <!-- Section -->
+
+    <!-- Slider Sec -->
+    <section class="inner_banner">
+        <div class="container">
+            <h1>Non-Interest Income</h1>
+            <ul class="breadcrum">
+                <li><a href="index.php">Home</a></li>
+                <li>Non-Interest Income</li>
+            </ul>
+        </div>
+    </section>
+
+    <section class="secondary-cards">
+        <div class="container">
+            <div class="row">
+
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="https://myleverage.com/solutions/overdraft-protection.php">
+                            <img src="assets/images/solutions/jmfa-card.png" alt="JMFA Overdraft Protection" />
+                            <div class="content">
+                                <h3>JMFA Overdraft Protection</h3>
+                                <div class="rollover">
+                                    <p>Earn non-interest income and provide a quality overdraft protection program to your members.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="https://www.membersecuritycenter.com/">
+                            <img src="assets/images/solutions/mbsc-card.png" alt="Member Security Center" />
+                            <div class="content">
+                                <h3>Member Security Center</h3>
+                                <div class="rollover">
+                                    <p>Member Security Center increases non-interest income and strengthens member relationships.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="https://myleverage.com/solutions/recordlyf.php">
+                            <img src="assets/images/solutions/record-card.png" alt="RecordLyf" />
+                            <div class="content">
+                                <h3>RecordLyf</h3>
+                                <div class="rollover">
+                                    <p>Protect your valuable personal information.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="https://myleveragerewards.com/#/">
+                            <img src="assets/images/solutions/rewards-card.png" alt="Leverage Rewards" />
+                            <div class="content">
+                                <h3>Leverage Rewards</h3>
+                                <div class="rollover">
+                                    <p>LEVERAGE Rewards is a valuable solution for increasing loyalty and achieving strategic objectives.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="https://myleverage.com/solutions/passtime.php">
+                            <img src="assets/images/solutions/passtime-card.png" alt="Stifel" />
+                            <div class="content">
+                                <h3>Stifel</h3>
+                                <div class="rollover">
+                                    <p>info to come.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="https://myleverage.com/solutions/css.php">
+                            <img src="assets/images/solutions/kasasa-card.png" alt="Kasasa" />
+                            <div class="content">
+                                <h3>Kasasa</h3>
+                                <div class="rollover">
+                                    <p>Kasasa is an innovative leader in financial technology and marketing services. They offer reward checking accounts and the first ever loan with takebacks. </p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="https://www.cusolutionsgroup.com/marketing/love-my-credit-union-rewards/love-my-credit-union-rewards">
+                            <img src="assets/images/solutions/lmcur-card.png" alt="Love My Credit Union Rewards" />
+                            <div class="content">
+                                <h3>Love My Credit Union Rewards</h3>
+                                <div class="rollover">
+                                    <p>Reward accounts, the only loan with take-backs, marketing & consulting services.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-sm-6">
+                    <div class="inner">
+                        <a class="event" href="https://myleverage.com/solutions/css.php">
+                            <img src="assets/images/solutions/zogo-card.png" alt="ZOGO" />
+                            <div class="content">
+                                <h3>ZOGO</h3>
+                                <div class="rollover">
+                                    <p>Zogo helps credit unions educate, engage, and attract young people. Backed by behavioral science research at Duke University, its gamified financial literacy app teaches sound financial fundamentals and rewards users for learning.</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+
+
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <footer>
+        <section class="footer_detail footer_detail_3">
+            <?php include 'assets/inc/footer.php'; ?>
+        </section>
+    </footer>
+    <!-- Js -->
+    <script src="assets/js/jquery-3.1.0.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/owl.carousel.js"></script>
+    <!-- theme Whatsapp chat -->
+    <script src="assets/js/aos.js"></script>
+    <script src="assets/js/script.js"></script>
+</body>
+
+</html>
