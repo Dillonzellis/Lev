223720556c3c1f586929808745f7ccee003ff671
Dillon Ellis <dellis@fcul.com>
2022-09-12 13:48:19 -0400

    original odp and odp-buisness

---
+++ solutions/odp-business-solutions.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>LEVERAGE - OBP Business Solutions</title>
+    <link rel="shortcut icon" type="image/x-icon" href="../assets/images/favicon.ico" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <!-- aos -->
+    <link href="../assets/css/aos.css" rel="stylesheet">
+    <!-- owl carousel -->
+    <link rel="stylesheet" href="../assets/css/owl.carousel.min.css">
+    <!-- style sheet -->
+    <link href="../assets/css/style.css" rel="stylesheet">
+<link href="../assets/css/scroll-style-inner.css" rel="stylesheet">
+    <!-- color stylesheet -->
+    <link id="skin-switcher-css" href="../assets/css/color-1.css" rel="stylesheet">
+    <!-- responsive stylesheet -->
+    <link href="../assets/css/responsive.css" rel="stylesheet">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-105142522-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-105142522-1');
+    </script>
+    <script type="text/javascript">
+        piAId = '514811';
+        piCId = '168940';
+        piHostname = 'pi.pardot.com';
+
+        (function() {
+            function async_load() {
+                var s = document.createElement('script');
+                s.type = 'text/javascript';
+                s.src = ('https:' == document.location.protocol ? 'https://pi' : 'http://cdn') + '.pardot.com/pd.js';
+                var c = document.getElementsByTagName('script')[0];
+                c.parentNode.insertBefore(s, c);
+            }
+            if (window.attachEvent) {
+                window.attachEvent('onload', async_load);
+            } else {
+                window.addEventListener('load', async_load, false);
+            }
+        })();
+    </script>
+
+
+    <style>
+        .bs-example {
+            margin: 20px;
+        }
+
+        .accordion .fa {
+            margin-right: 0.5rem;
+        }
+
+        h2 a {
+            text-decoration: underline !important;
+        }
+    </style>
+    <script>
+        $(document).ready(function() {
+            // Add minus icon for collapse element which is open by default
+            $(".collapse.show").each(function() {
+                $(this).prev(".card-header").find(".fa").addClass("fa-minus").removeClass("fa-plus");
+            });
+
+            // Toggle plus minus icon on show hide of collapse element
+            $(".collapse").on('show.bs.collapse', function() {
+                $(this).prev(".card-header").find(".fa").removeClass("fa-plus").addClass("fa-minus");
+            }).on('hide.bs.collapse', function() {
+                $(this).prev(".card-header").find(".fa").removeClass("fa-minus").addClass("fa-plus");
+            });
+        });
+    </script>
+	
+	<!-- Remodal styles -->
+    <link rel="stylesheet" href="../remodal/dist/remodal.css">
+    <link rel="stylesheet" href="../remodal/dist/remodal-default-theme.css">
+</head>
+
+<body>
+    <!-- Header -->
+    <header>
+        <div class="nav_sec">
+            <div class="container">
+                <?php include '../assets/inc/nav.php'; ?>
+            </div>
+        </div>
+    </header>
+    <!-- Section -->
+
+    <!-- Slider Sec -->
+    <section class="inner_banner">
+        <div class="container">
+            <h1><img src="../assets/images/ODP_Logo_WHITE_RGB_Stacked.png" alt="OBP Business Solutions" width="464" height="164" class="img-fluid"></h1>
+            <ul class="breadcrum">
+                <li><a href="../index.php">Home</a></li>
+                <li><a href="../solutions.php">Solutions</a></li>
+                <li>OBP Business Solutions</li>
+            </ul>
+				
+				
+				
+			</div>
+		</section>
+    <!-- About Sec -->
+    <section class="about_sec service_sec2">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-md-8">
+                    <div class="detail">
+
+                        <h2><a href="../assets/docs/ODP-BS Office Supplies Product Sheet CU 041922.pdf" target="_blank" rel="noopener noreferrer">Office Supplies</a></h2>
+
+                        <p>ODP Business Solutions (formerly known as Office Depot Business Solutions) and LEVERAGE offer a National Buying Program that will maximize savings to Credit Unions of all sizes through collaborative buying power. Higher volumes to ODP Business Solutions and LEVERAGE result in higher savings to Credit Unions and its Members!</p>
+						
+					    <p align="center"><iframe width="560" height="315" src="https://www.youtube.com/embed/W3AcVoLEsN0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></p>
+						
+                       <p><strong style="font-weight: 700;">Sign Up for Savings!</strong></p>
+						<p><a href="https://community.officedepot.com/GPOHome?id=54512610" target="_blank" class="myButton">Credit Union Registration</a> &nbsp;&nbsp;&nbsp;&nbsp;<a href="https://community.officedepot.com/GPOHome?id=46702310" target="_blank" class="myButton">Credit Union Member Registration</a></p>
+						
+						<p>*Attn: Credit Union Representatives – Increase loyalty by co-branding and sending your members this <a href="../assets/docs/Office-Depot-Product-Sheet-CUMembers.pdf" target="_blank" style="font-weight: 700; text-decoration: underline;">flyer</a> so 
+   they can take advantage of the very same savings your credit union will experience.</p>
+						
+                        <h2>Workspace Interiors</h2>
+
+                        <p>Looking for workplace solutions that help redefine how your employee work and help create a mindset that reflects the culture of your company. Look no further than Workspace Interiors by ODP Business Solutions.
+                        </p>
+                    </div>
+                </div>
+				
+				<div class="col-lg-4 col-md-4">
+
+                    <p><a href="mailto:consult@myleverage.com" class="myButton" style="width: 90%;">Contact Us</a></p>
+
+                    <!--<h2>Resources</h2>
+						
+					<p><a href="../assets/docs/Glia-Slick.pdf" target="_blank" class="myButton" style="width: 90%;">Product Sheet</a></p>
+						<p><a href="https://youtu.be/xQScLTMWk6g" target="_blank" class="myButton" style="width: 90%;">Video</a></p>-->
+
+                    <p><a href="#" data-remodal-target="form" class="myButton" style="width: 90%;">Request Resources</a></p>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <footer>
+        <section class="footer_detail footer_detail_3">
+            <?php include '../assets/inc/footer.php'; ?>
+        </section>
+    </footer>
+    <!-- Js -->
+    <script src="../assets/js/owl.carousel.js"></script>
+    <!-- theme Whatsapp chat -->
+    <script src="../assets/js/script.js"></script>
+	<script src="../remodal/remodal.js"></script>
+    <script src="../remodal/remodal.min.js"></script>
+
+    <!--REMODALS-->
+    <div class="remodal" data-remodal-id="form">
+        <a data-remodal-action="close" class="remodal-close"></a>
+        <h3>Request Resources</h3>
+        <p>Resources include:</p>
+
+        <ul style="list-style: disc; padding-left: 25px;">
+            <li>• Product Sheet: Office Supplies</li>
+            <li>• Product Sheet: Workspace Interiors</li>
+        </ul>
+
+        <p>To fulfill your request, please provide the following:</p>
+        <p><em>* = Required field.</em></p>
+        <p align="center"><iframe src="https://pardot.lscu.coop/l/513811/2022-02-09/3tbr34m" width="100%" height="500" type="text/html" frameborder="0" allowTransparency="true" style="border: 0"></iframe></p>
+        <a data-remodal-action="cancel" class="remodal-cancel" href="#">Close</a>
+    </div>
+</body>
+
+</html>
---
+++ solutions/odp.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		
+		<title>LEVERAGE -ODP</title>
+		<link rel="shortcut icon" type="image/x-icon" href="../assets/images/favicon.ico" />
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+		<!-- aos -->
+		<link href="../assets/css/aos.css" rel="stylesheet">
+		<!-- owl carousel -->
+		<link rel="stylesheet" href="../assets/css/owl.carousel.min.css">
+		<!-- style sheet -->
+		<link href="../assets/css/style.css" rel="stylesheet">
+<link href="../assets/css/scroll-style-inner.css" rel="stylesheet">
+		<!-- color stylesheet -->
+		<link id="skin-switcher-css" href="../assets/css/color-1.css" rel="stylesheet">
+		<!-- responsive stylesheet -->
+		<link href="../assets/css/responsive.css" rel="stylesheet">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-105142522-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-105142522-1');
+</script>
+		<script type="text/javascript">
+piAId = '514811';
+piCId = '168940';
+piHostname = 'pi.pardot.com';
+
+(function() {
+	function async_load(){
+		var s = document.createElement('script'); s.type = 'text/javascript';
+		s.src = ('https:' == document.location.protocol ? 'https://pi' : 'http://cdn') + '.pardot.com/pd.js';
+		var c = document.getElementsByTagName('script')[0]; c.parentNode.insertBefore(s, c);
+	}
+	if(window.attachEvent) { window.attachEvent('onload', async_load); }
+	else { window.addEventListener('load', async_load, false); }
+})();
+</script>
+
+		
+		<style>
+    .bs-example{
+        margin: 20px;
+    }
+    .accordion .fa{
+        margin-right: 0.5rem;
+    }
+</style>
+<script>
+    $(document).ready(function(){
+        // Add minus icon for collapse element which is open by default
+        $(".collapse.show").each(function(){
+        	$(this).prev(".card-header").find(".fa").addClass("fa-minus").removeClass("fa-plus");
+        });
+        
+        // Toggle plus minus icon on show hide of collapse element
+        $(".collapse").on('show.bs.collapse', function(){
+        	$(this).prev(".card-header").find(".fa").removeClass("fa-plus").addClass("fa-minus");
+        }).on('hide.bs.collapse', function(){
+        	$(this).prev(".card-header").find(".fa").removeClass("fa-minus").addClass("fa-plus");
+        });
+    });
+</script>
+	</head>
+	<body>
+		<!-- Header -->
+		<header>
+			<div class="nav_sec">
+				<div class="container">
+					<?php include '../assets/inc/nav.php';?>
+				</div>
+			</div>
+		</header>
+		<!-- Section -->
+		
+		<!-- Slider Sec -->
+		<section class="inner_banner">
+			<div class="container">
+				<h1><img src="../assets/images/logos/ODP_Logo_WHITE_RGB_Stacked.png" alt="ODP logo" width="340" height="169" class="img-fluid"></h1>
+				<ul class="breadcrum">
+					<li><a href="../index.php">Home</a></li>
+					<li><a href="../solutions.php">Solutions</a></li>
+					<li>Glia</li>
+				</ul>
+				
+				
+				
+			</div>
+		</section>
+		<!-- About Sec -->
+		<section class="about_sec service_sec2">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 col-md-12">
+						<div class="detail">
+				
+						  <p>ODP Business Solutions (formerly known as Office Depot Business Solutions) and LEVERAGE offer a National Buying Program that will maximize savings to Credit Unions of all sizes through collaborative buying power. Higher volumes to ODP Business Solutions and LEVERAGE result in higher savings to Credit Unions!</p>
+							
+							<p><strong style="font-weight: 600;">Credit Union Program Benefits:</strong></p>
+
+							
+						  <ul style="list-style: disc; padding-left: 25px;">
+							  <li>• <strong style="font-weight: 600;">10% - 45%</strong> - Off on Office Products and Supplies</li>
+							  <li>• <strong style="font-weight: 600;">20% - 45%</strong> - Off on Cleaning, Facility, and Breakroom Items</li>
+							  <li>• <strong style="font-weight: 600;">10% - 15%</strong> - Off on Office Furniture</li>
+							  <li>• <strong style="font-weight: 600;">20%</strong> - Off on Private Brand Ink/Toner<sup>(a)</sup></li>
+							  <li>• <strong style="font-weight: 600;">10%</strong> - Off on Technology Solutions<sup>(b)</sup></li>
+							  <li>• <strong style="font-weight: 600;">Free Next Day Delivery</strong> on $50 minimum order</li>
+			              </ul>
+							
+							<p style="font-size: 10px;">(a) 10% or more off national labels like Hewlett Packard<br>
+							(b) With competitively bid options</p>
+							
+						  <p><strong style="font-weight: 600;">Order for Delivery or In-Store Pickup via:</strong></p>
+							
+							<ul style="list-style: disc; padding-left: 25px;">
+							  <li>• Online</li>
+								<li>• Phone</li>
+			              </ul>
+							
+							<p><strong style="font-weight: 600;">Additional Key Credit Union Program Features Include:</strong></p>
+							
+							<ul style="list-style: disc; padding-left: 25px;">
+							  <li>• Extension of same savings as a benefit to Credit Union Members</li>
+								<li>• Selecting "Custom Core" products with deeper discounts</li>
+								<li>• Competitive bidding for Credit Unions with over 100 employees</li>
+								<li>• "Mobile Optimization" to place, hold, and release branch orders</li>
+								<li>• Utilization of live fee "Dashboard" by branch<sup>(c)</sup></li>
+								<li>• Subscription offerings for regularly ordered products<sup>(d)</sup></li>
+			              </ul>
+							
+						  <p style="font-size: 10px;">(c) Track product spending and savings as well as compare products and savings<br>
+						  (d) Weekly, bi-weekly, or monthly automated-ordering subscription</p>
+							
+							<p><a href="mailto:consult@myleverage.com" class="myButton">Contact Us!</a> &nbsp;&nbsp;&nbsp;&nbsp;<a href="../assets/docs/Office-Depot-Product-Sheet-CU.pdf" target="_blank" class="myButton">Resources</a></p>
+
+
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- Footer -->
+		<footer>
+			<section class="footer_detail footer_detail_3">
+				<?php include '../assets/inc/footer.php';?>
+			</section>
+		</footer>
+		<!-- Js -->
+		<script src="../assets/js/owl.carousel.js"></script>
+		<!-- theme Whatsapp chat -->
+		<script src="../assets/js/script.js"></script>
+	</body>
+</html>
